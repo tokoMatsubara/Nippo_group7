19e0135a2f6157fcc11ed7d31c507260fc7de301
WIN-62FS9LIDB6I\traininguser <tomohiro.takaki@skywill.jp>
2026-06-02 16:36:03 +0900

    日報データ取得ロジック実装途中

--- backend/src/main/java/com/daily_app/demo/Repository/DailyDetailRepository.java
+++ backend/src/main/java/com/daily_app/demo/Repository/DailyDetailRepository.java
@@ -1,11 +1,34 @@
 package com.daily_app.demo.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.daily_app.demo.Dto.DailiesQueryDto;
 import com.daily_app.demo.Entity.DailyDetail;
 
 @Repository
 public interface DailyDetailRepository extends JpaRepository<DailyDetail, Long> {
-    
+
+    @Query("""
+            SELECT 
+                d.daily_id,
+                d.created_at,
+                c.category_id,
+                c.category_name,
+                dd.content,
+            FROM dailies AS d
+            JOIN daily_details AS dd
+            ON d.daily_id = dd.daily_id
+            JOIN categories AS c 
+            ON c.category_id = dd.category_id
+            JOIN users AS u
+            ON u.user_id = d.user_id
+            WHERE u.user_id = :userId
+            AND d.created_at BETWEEN :startDate AND :endDate
+            """)
+        List<DailiesQueryDto> dailiesContentList(Integer userId, LocalDate startDate, LocalDate endDate)
 }
--- backend/src/main/java/com/daily_app/demo/Service/DailyCrudService.java
+++ backend/src/main/java/com/daily_app/demo/Service/DailyCrudService.java
@@ -6,17 +6,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.daily_app.demo.Dto.Response.DailyResponseDto;
 import com.daily_app.demo.Dto.Response.WeeklyListResponseDto;
 import com.daily_app.demo.Dto.Response.WeeklyListResponseDto.SummaryDto;
 import com.daily_app.demo.Entity.WeeklySummary;
+import com.daily_app.demo.Repository.DailyDetailRepository;
 import com.daily_app.demo.Repository.WeeklySummaryRepository;
 
 public class DailyCrudService {
 
     @Autowired
     private WeeklySummaryRepository weeklySummaryRepository;
+    @Autowired
+    private DailyDetailRepository dailyDetailrepository;
 
     //weeklyResponse ====================================================================
+
     public WeeklyListResponseDto weeklyListResponse(Integer userId){
         List<WeeklySummary> weeklySummaries = weeklySummaryRepository.findByUserId(userId);
         return weeklyListResponse(weeklySummaries);
@@ -37,7 +42,11 @@ public class DailyCrudService {
         return new WeeklyListResponseDto(dtos);
     }
 
-    
+    //dailyReponse=============================================================
+
+    public DailyResponseDto dailyResponse(Integer userId, LocalDate startDate, LocalDate endDate){
+        
+    }
 
     
 }
