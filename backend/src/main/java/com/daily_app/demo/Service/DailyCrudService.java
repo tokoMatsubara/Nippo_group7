91dba2382dd73345db03ec389a16929b9417a75f
WIN-62FS9LIDB6I\traininguser <tomohiro.takaki@skywill.jp>
2026-06-02 15:42:31 +0900

    週要約取得のAPIロジック実装

--- backend/src/main/java/com/daily_app/demo/Service/DailyCrudService.java
+++ backend/src/main/java/com/daily_app/demo/Service/DailyCrudService.java
@@ -1,5 +1,43 @@
 package com.daily_app.demo.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.daily_app.demo.Dto.Response.WeeklyListResponseDto;
+import com.daily_app.demo.Dto.Response.WeeklyListResponseDto.SummaryDto;
+import com.daily_app.demo.Entity.WeeklySummary;
+import com.daily_app.demo.Repository.WeeklySummaryRepository;
+
 public class DailyCrudService {
+
+    @Autowired
+    private WeeklySummaryRepository weeklySummaryRepository;
+
+    //weeklyResponse ====================================================================
+    public WeeklyListResponseDto weeklyListResponse(Integer userId){
+        List<WeeklySummary> weeklySummaries = weeklySummaryRepository.findByUserId(userId);
+        return weeklyListResponse(weeklySummaries);
+    }
+
+    private SummaryDto weeklySummaryEntityToDto(WeeklySummary weeklySummary){
+        LocalDate startDate = weeklySummary.getWeekStartDate();
+        LocalDate endDate = weeklySummary.getWeekEndDate();
+        String content = weeklySummary.getWeeklySummaryContent();
+
+        return new SummaryDto(startDate, endDate, content);
+    }
+    private WeeklyListResponseDto weeklyListResponse(List<WeeklySummary> summaries){
+        List<SummaryDto> dtos = new ArrayList<SummaryDto>();
+        for (WeeklySummary summary : summaries) {
+            dtos.add(weeklySummaryEntityToDto(summary));
+        }
+        return new WeeklyListResponseDto(dtos);
+    }
+
+    
+
     
 }
