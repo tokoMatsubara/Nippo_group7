cdc44fd67b8cf42892561659b131b1a46679c77b
WIN-8JI6590HBCV\traininguser <toko.matsubara@skywill.jp>
2026-06-02 14:51:24 +0900

    これでどーだ

--- backend/src/main/java/com/daily_app/demo/DemoApplication.java
+++ backend/src/main/java/com/daily_app/demo/DemoApplication.java
@@ -2,9 +2,14 @@ package com.daily_app.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 // @SpringBootApplication
-@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    SecurityAutoConfiguration.class
+})
 public class DemoApplication {
 
 	public static void main(String[] args) {
