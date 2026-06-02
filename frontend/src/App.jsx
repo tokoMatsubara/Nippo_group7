bccc1a15e64309097d1499724db5b9f23b1abee7
WIN-8JI6590HBCV\traininguser <toko.matsubara@skywill.jp>
2026-06-02 11:06:48 +0900

    Remind雛型作成完了by松原

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
+
 import CreateReport from "./pages/CreateReport";
+import Remind from "./pages/Remind";
 
 function App() {
-  return <CreateReport />;
+  return (
+    <>
+      {/* 日報作成画面 */}
+      {/* <CreateReport /> */}
+
+      {/* リマインダー画面 */}
+      <Remind />
+    </>
+  );
 }
 
 export default App;
