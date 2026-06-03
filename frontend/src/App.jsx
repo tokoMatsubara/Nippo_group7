db0693e3fb0ca0cbdae0d0ffc7651a7af62c2683
WIN-8JI6590HBCV\traininguser <toko.matsubara@skywill.jp>
2026-06-03 10:01:10 +0900

    APP.jsxのconflictを解決

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import CreateReport from "./pages/CreateReport";
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import CreateReport from "./pages/CreateReport";
 import DailyList from "./pages/DailyList";
->>>>>>> 66bebb8f3530495b406748372764f67a9db11f8b
 import Remind from "./pages/Remind";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         <Route
           path="/create-report"
           element={<CreateReport />}
@@ -28,7 +22,6 @@ function App() {
           path="/remind"
           element={<Remind />}
         />
-=======
 
         {/* 認証系 */}
         <Route path="/login" element={<Login />} />
@@ -47,7 +40,6 @@ function App() {
         {/* デフォルト */}
         <Route path="*" element={<Login />} />
 
->>>>>>> 66bebb8f3530495b406748372764f67a9db11f8b
       </Routes>
     </BrowserRouter>
   );
