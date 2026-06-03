03254184cffade0e6042937032df51b7c1453786
WIN-8JI6590HBCV\traininguser <toko.matsubara@skywill.jp>
2026-06-03 12:01:02 +0900

    UserRepository.javaを編集した

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+//import Dashboard from "./pages/Dashboard";
 import CreateReport from "./pages/CreateReport";
-import DailyList from "./pages/DailyList";
+//import DailyList from "./pages/DailyList";
 import Remind from "./pages/Remind";
 
 function App() {
@@ -24,15 +24,16 @@ function App() {
         />
 
         {/* 認証系 */}
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login"/>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register />} />
 
-        {/* メイン */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* メイン 
+        <Route path="/dashboard" element={<Dashboard />} />*/}
 
-        {/* 日報系 */}
+        {/* 日報系 
         <Route path="/create-report" element={<CreateReport />} />
-        <Route path="/daily-list" element={<DailyList />} />
+        <Route path="/daily-list" element={<DailyList />} />*/}
 
         {/* リマインド */}
         <Route path="/remind" element={<Remind />} />
@@ -47,3 +48,31 @@ function App() {
 
 
 export default App;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import Login from "./pages/login"; 
+// // ➕ 【追加1】新規登録画面のファイルをインポートする（ファイル名やパスに合わせてね）
+// import Register from "./pages/register"; 
+
+// function App() {
+//   return (
+//     <Router>
+//       <div style={{ background: "black", color: "white", minHeight: "100vh", padding: "20px" }}>
+//         <p style={{ fontWeight: "bold", color: "blue" }}>★Reactの通信準備が完了しました！</p>
+//         <hr />
+        
+//         <Routes>
+//           <Route path="/" element={<Navigate to="/login" />} />
+//           <Route path="/login" element={<Login />} />
+          
+//           {/* ➕ 【追加2】/register にアクセスされたときのルールを追加 */}
+//           <Route path="/register" element={<Register />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
