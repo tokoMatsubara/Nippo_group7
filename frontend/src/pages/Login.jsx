c582c13ff757b160124e6e3be032f0b7ae3a2a25
WIN-MNS33TQT9HN\traininguser <kohei.imafuji@skywill.jp>
2026-06-04 11:41:52 +0900

    console文が二つあったので一つ消しました。

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -32,7 +32,6 @@ export default function Login() {
             }
 
             const data = await res.json();
-            console.log("LOGIN RESPONSE:", data);
 
             console.log("ログイン成功:", data);
 
