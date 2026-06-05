1dc58d9a88e4b52003a2e326052745f48185dabb
WIN-MNS33TQT9HN\traininguser <kohei.imafuji@skywill.jp>
2026-06-05 14:56:27 +0900

    SET文追加

--- setup.sql
+++ setup.sql
@@ -77,6 +77,7 @@ CREATE TABLE weekly_summaries (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+SET client_encoding TO 'UTF8';
 -- カテゴリ登録
 INSERT INTO categories(category_name) VALUES 
     ('今日学んだこと'),
