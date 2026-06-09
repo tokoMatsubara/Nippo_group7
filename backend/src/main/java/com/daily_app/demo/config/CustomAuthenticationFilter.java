d6142fe55ea1318ba2b0ea2477f141ca00e6976b
WIN-62FS9LIDB6I\traininguser <tomohiro.takaki@skywill.jp>
2026-06-09 11:25:49 +0900

    AuthenticationFilterの仮実装完了

---
+++ backend/src/main/java/com/daily_app/demo/config/CustomAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.daily_app.demo.config;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.daily_app.demo.Service.CustomUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthenticationFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterchain)
+        throws ServletException, IOException{
+        Cookie cookies[] = request.getCookies();
+        if(cookies.length <= 0){
+            String email = Arrays.stream(cookies)
+                .filter(c -> "email".equals(c.getName()))
+                .map(c -> c.getValue())
+                .findFirst()
+                .orElse(null);
+            String password = Arrays.stream(cookies)
+                .filter(c -> "password".equals(c.getName()))
+                .map(c -> c.getValue())
+                .findFirst()
+                .orElse(null);  
+            
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+                
+            UsernamePasswordAuthenticationToken token = 
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+
+        filterchain.doFilter(request, response);
+    }
+}
