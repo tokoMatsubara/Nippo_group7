74e431d2c5c100fff0167c2318b45eaccef9f76e
WIN-62FS9LIDB6I\traininguser <tomohiro.takaki@skywill.jp>
2026-06-10 15:27:38 +0900

    Remindの変数名が一致しない問題を修正

--- frontend/src/pages/Remind.jsx
+++ frontend/src/pages/Remind.jsx
@@ -56,8 +56,8 @@ function Remind() {
       console.log(settingData);
 
       //const goal = data[0].remindContent;があった場所。履歴を全部渡すためにここ変えます
-      const latestGoal = data[0]?.remindContent;
-      const historyGoals = data
+      const latestGoal = remindData[0]?.remindContent;
+      const historyGoals = remindData
         .slice(1)
         .map(item => item.remindContent);
 
